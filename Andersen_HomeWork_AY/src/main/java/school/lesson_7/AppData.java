45469140d34dbcd01696b7601f5a27a83de69b64
Ärtiom Yeshchenko <eshchenko@gmail.com>
2022-06-13 23:21:11 +0400

    added homework No.7

--- Andersen_HomeWork_AY/src/main/java/school/lesson_7/AppData.java
+++ Andersen_HomeWork_AY/src/main/java/school/lesson_7/AppData.java
@@ -1,5 +1,10 @@
 package school.lesson_7;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class AppData {
     private String[] header;
     private int[][] data;
@@ -9,8 +14,33 @@ public class AppData {
         this.data = data;
     }
 
-    public static void save(AppData data) {
+    // getData генерирует String для записи в файл
+    public static String generateData(AppData data) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < data.header.length; i++) {
+            builder.append(data.header[i]);
+            if (i != data.header.length - 1) {
+                builder.append(';');
+            } else {
+                builder.append('\n');
+            }
+        }
+        for (int i = 0; i < data.data.length; i++) {
+            for (int j = 0; j < data.data[i].length; j++) {
+                builder.append(data.data[i][j]);
+                if (j < data.data[i].length - 1) {
+                    builder.append(';');
+                }
+            }
+            builder.append('\n');
+        }
+        return new String(builder);
+    }
 
+    public static void save(AppData data) throws IOException {
+        BufferedWriter csvWriter = new BufferedWriter(new FileWriter("HW.csv"));
+        csvWriter.write(generateData(data));
+        csvWriter.close();
     }
 
     public String[] getHeader() {
@@ -27,5 +57,32 @@ public class AppData {
 
     public void setData(int[][] data) {
         this.data = data;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        AppData appData = (AppData) o;
+
+        // Probably incorrect - comparing Object[] arrays with Arrays.equals
+        if (!Arrays.equals(header, appData.header)) return false;
+        return Arrays.deepEquals(data, appData.data);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Arrays.hashCode(header);
+        result = 31 * result + Arrays.deepHashCode(data);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "AppData{" +
+                "header=" + Arrays.toString(header) +
+                ", data=" + Arrays.toString(data) +
+                '}';
     }
 }
